26747933c051758c0c4751e8fa292ae0f5223005
ehuggett <ehuggett@users.noreply.github.com>
2017-08-25 20:55:11 +0100

    add password_compat library for password_* functions

--- public_html/lists/admin/index.php
+++ public_html/lists/admin/index.php
@@ -23,6 +23,9 @@ require_once dirname(__FILE__).'/inc/unregister_globals.php';
 require_once dirname(__FILE__).'/inc/magic_quotes.php';
 // Remove when php5.X is unsupported, currently 31 Dec 2018, https://secure.php.net/supported-versions.php
 require_once dirname(__FILE__).'/inc/random_compat/random.php';
+// Security fixes for php5.4 ended on 3 Sep 2015
+// Remove when php5.4 is unsupported
+require_once dirname(__FILE__).'/inc/password_compat.php';
 
 /* no idea why it wouldn't be there (no dependencies are mentioned on php.net/mb_strtolower), but
  * found a system missing it. We need it from the start */
